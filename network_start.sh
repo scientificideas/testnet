995c1677af9d6539e6cc37e42c45ac28befb70be
Artem Barger <artem@bargr.net>
2021-12-28 03:04:34 +0200

    Fix configtx.yaml
    Signed-off-by: Artem Barger <artem@bargr.net>

--- network_start.sh
+++ network_start.sh
@@ -2,5 +2,6 @@
 
 cryptogen generate --config crypto-config.yaml --output crypto
 configtxgen -configPath . -profile Genesis -channelID system -outputBlock genesis.block
+configtxgen -configPath . -profile MyChannel -channelID mychannel -outputCreateChannelTx channel.tx
 
 docker-compose up -d
