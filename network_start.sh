0485daaf66fa137b3821b7b1f158320ed2db2145
Artem Barger <artem@bargr.net>
2022-01-11 01:04:06 +0200

    Use fabric-tools rather than relying on binary being available
    Signed-off-by: Artem Barger <artem@bargr.net>

--- network_start.sh
+++ network_start.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-cryptogen generate --config crypto-config.yaml --output crypto
-configtxgen -configPath . -profile Genesis -channelID system -outputBlock genesis.block
-configtxgen -configPath . -profile MyChannel -channelID mychannel -outputCreateChannelTx channel.tx
+DOCKER=docker
+TOOLS_IMAGE=hyperledger/fabric-tools
+
+$DOCKER run --rm -v $(pwd):"/home/admin" -w "/home/admin" $TOOLS_IMAGE cryptogen generate --config crypto-config.yaml --output crypto
+$DOCKER run --rm -v $(pwd):"/home/admin" -w "/home/admin" $TOOLS_IMAGE configtxgen -configPath . -profile Genesis -channelID system -outputBlock genesis.block
+$DOCKER run --rm -v $(pwd):"/home/admin" -w "/home/admin" $TOOLS_IMAGE configtxgen -configPath . -profile MyChannel -channelID mychannel -outputCreateChannelTx channel.tx
 
 docker-compose up -d
